fd5390c150a324d702ee2298cdd4a2fb7240ef6e
Cats1337 <42129397+Cats1337@users.noreply.github.com>
2024-05-17 22:46:44 -0400

    yml & years

--- portfolioAssets/js/imageMap.js
+++ portfolioAssets/js/imageMap.js
@@ -1,13 +1,14 @@
-// Fetch the language data from the JSON file
+// Fetch the language data from the YAML file
 async function fetchLanguages() {
   try {
-    const response = await fetch('portfolioAssets/json/languages.json');
+    const response = await fetch('portfolioAssets/yaml/languages.yaml');
 
     if (!response.ok) {
       throw new Error(`Network response was not ok. Status: ${response.status}`);
     }
 
-    const categories = await response.json();
+    const yamlText = await response.text();
+    const categories = jsyaml.load(yamlText);
 
     // Display the languages with corresponding images
     displayLanguages(categories);
@@ -40,6 +41,12 @@ function createParagraph(text) {
   return p;
 }
 
+// Function to calculate the years since a given year
+function calculateYearsSince(year) {
+  const currentYear = new Date().getFullYear();
+  return currentYear - year;
+}
+
 // Function to display languages with corresponding images
 function displayLanguages(categories) {
   const imageMapsContainer = document.getElementById('imageMaps');
@@ -57,22 +64,25 @@ function displayLanguages(categories) {
       categoryDiv.appendChild(categoryHeading);
 
       // Display the languages in groups of 4
-      for (let i = 0; i < languages.length; i += 4) {
+      const languageEntries = Object.entries(languages);
+      for (let i = 0; i < languageEntries.length; i += 4) {
         const languagesContainer = document.createElement('div');
         languagesContainer.classList.add('languages-container');
 
-        for (let j = i; j < i + 4 && j < languages.length; j++) {
+        for (let j = i; j < i + 4 && j < languageEntries.length; j++) {
+          const [language, year] = languageEntries[j];
           const languageDiv = document.createElement('div');
           languageDiv.classList.add('language');
 
           // Transform the code name to match the image file name
-          const transformedCodeName = transformCodeName(languages[j]);
+          const transformedCodeName = transformCodeName(language);
 
           // If the language is not in the devicon library, use custom image
           if (transformedCodeName === 'cobol') {
-            const image = createImage(`portfolioAssets/images/${transformedCodeName}.svg`, languages[j]);
+            const image = createImage(`portfolioAssets/images/${transformedCodeName}.svg`, language);
             languageDiv.appendChild(image);
-            languageDiv.appendChild(createParagraph(languages[j]));
+            languageDiv.appendChild(createParagraph(language));
+            languageDiv.appendChild(createParagraph(`${year} - ${calculateYearsSince(year)} years`));
             languagesContainer.appendChild(languageDiv);
             continue;
           }
@@ -81,25 +91,27 @@ function displayLanguages(categories) {
           if (transformedCodeName === 'mysql') {
             const imageFolderName = `${transformedCodeName}`;
             const imageFileName = `${transformedCodeName}-original-wordmark.svg`;
-            const image = createImage(`https://raw.githubusercontent.com/devicons/devicon/master/icons/${imageFolderName}/${imageFileName}`, languages[j]);
+            const image = createImage(`https://raw.githubusercontent.com/devicons/devicon/master/icons/${imageFolderName}/${imageFileName}`, language);
             languageDiv.appendChild(image);
-            languageDiv.appendChild(createParagraph(languages[j]));
+            languageDiv.appendChild(createParagraph(language));
+            languageDiv.appendChild(createParagraph(`${year} - ${calculateYearsSince(year)} years`));
             languagesContainer.appendChild(languageDiv);
             continue;
           }
 
-          // get the image from the devicon library
+          // Get the image from the devicon library
           const imageFolderName = `${transformedCodeName}`;
           const imageFileName = `${transformedCodeName}-original.svg`;
 
           // Create the image element and append it to the language div
-          const image = createImage(`https://raw.githubusercontent.com/devicons/devicon/master/icons/${imageFolderName}/${imageFileName}`, languages[j]);
+          const image = createImage(`https://raw.githubusercontent.com/devicons/devicon/master/icons/${imageFolderName}/${imageFileName}`, language);
           languageDiv.appendChild(image);
-          languageDiv.appendChild(createParagraph(languages[j]));
+          languageDiv.appendChild(createParagraph(language));
+          languageDiv.appendChild(createParagraph(`${year} - ${calculateYearsSince(year)} years`));
           languagesContainer.appendChild(languageDiv);
         }
 
-        const numberOfLanguages = languages.length - i;
+        const numberOfLanguages = languageEntries.length - i;
         if (numberOfLanguages < 4) {
           languagesContainer.style.justifyContent = 'center';
           languagesContainer.style.display = 'flex';
